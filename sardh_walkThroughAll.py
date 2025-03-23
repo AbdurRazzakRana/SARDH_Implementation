05d5cb386f0faecaca6270ad44017a73428b3489
AbdurRazzakRana <razzakrana17@gmail.com>
2025-03-23 15:10:39 -0600

    array reference genrated and reuse calculated, mood 3 and 1 implemented

--- sardh_walkThroughAll.py
+++ sardh_walkThroughAll.py
@@ -5,11 +5,17 @@ from t3_create_setn_vector_n_rf import create_set_n_vector_n_rf
 from t4_unresolved_refs_solve import unresolved_refs_solve
 from t5_equation_n_predict_actual_bound import predict_2_nested, predict_3_nested
 from t6_array_lookup_table import array_lookup_table
+from t7_calc_array_reference_rd import calc_array_reference_rd
 
 import time
+
+# 1 = completely static prediction
+# 2 = set an avg to the later arrays
+# 3 = generating the array sequence
+mood = 1
 start_time = time.time()
 
-vec_refs = ['retval', 'i', '[4', 'i', 'j', '[4', 'j', 'i', 'i', 'j', 'arr-i-j', 'i', 'i', 'brr-i', 'j', 'j', ']', 'j', 'i', 'i', ']', 'i', 'i', '[5', 'i', 'j', '[6', 'j', 'i', 'i', 'j', 'arr-i-j', 'i', 'i', 'brr-i', 'j', 'j', ']', 'j', 'i', 'i', ']', 'i']
+vec_refs = ['retval', 'i', '[100', 'i', 'j', '[200', 'j', 'i', 'i', 'j', 'arr-i-j', 'i', 'i', 'brr-i', 'j', 'j', ']', 'j', 'i', 'i', ']', 'i', 'i', '[300', 'i', 'j', '[400', 'j', 'i', 'i', 'j', 'arr-i-j', 'i', 'i', 'brr-i', 'j', 'j', ']', 'j', 'i', 'i', ']', 'i']
 final_rf = {}
 # part2: array reference resolve.
 array_refs_total = []
@@ -64,11 +70,11 @@ for item in separated_loop_blocks:
 
         if len(loop_bounds) == 2:
             pred_rf = predict_2_nested(key_dict=key_dict, i_tot=loop_bounds[0][0]-2, j_tot=loop_bounds[1][0]-2)
-            print(pred_rf)
+            # print(pred_rf)
             # print("2 nested loop")
         elif len(loop_bounds) == 3:
             pred_rf = predict_3_nested(key_dict=key_dict, i_tot=loop_bounds[0][0]-2, j_tot=loop_bounds[1][0]-2, k_tot=loop_bounds[2][0]-2)
-            print(pred_rf)
+            # print(pred_rf)
             # print("3 nested loop")
         
         for key, value in pred_rf.items():
@@ -76,20 +82,30 @@ for item in separated_loop_blocks:
                 final_rf[key] += value
             else:
                 final_rf[key] = value
-        # print(loop_bounds)
-        array_references, const_refs = array_lookup_table(array_rfs_one_loop, loop_bounds[0][0], loop_bounds[1][0])
-        
-        if not array_refs_total:
-            print("first loop adding up all the references")
-            array_refs_total.append(const_refs)
-            array_refs_total.append(array_references)
-            print(array_refs_total)
-        else:
-            # calc_array_reference_rds(array_refs_total, const_refs, array_references)
-            print("Function under connstruction")
-            print(f"constant variables at this level: {const_refs}")
-            print(f"Array Variables: {array_references}")
-        print("--------------------------------------------")
+        if mood == 3:
+            array_references, const_refs = array_lookup_table(array_rfs_one_loop, loop_bounds[0][0], loop_bounds[1][0])
+            
+            if not array_refs_total:
+                # print("first loop adding up all the references")
+                array_refs_total.append(const_refs)
+                array_refs_total.append(array_references)
+                array_refs_total.append(set(array_references))
+                # print(array_refs_total)
+            else:
+                array_rf, array_refs_total[0],array_refs_total[1], array_refs_total[2] = calc_array_reference_rd(array_refs_total, array_references, const_refs)
+                #as the array prediction is already counted from the estimation, adds all the newly discovered rds and deduct from the cold misses
+                count_resolved_array_rfs = 0
+                for key, value in array_rf.items():
+                    if key in final_rf:
+                        final_rf[key] += value
+                    else:
+                        final_rf[key] = value
+                    count_resolved_array_rfs += value
+                final_rf[-1] -= count_resolved_array_rfs  
+                print(f"did nothing with:{array_rf[-1]}")
+        # elif mood == 1:
 
+end_time = time.time()
 print("Final RP:")
 print(final_rf)
+print(f"Time taken: {end_time-start_time}")
--- t7_calc_array_reference_rd.py
+++ t7_calc_array_reference_rd.py
@@ -1,38 +1,48 @@
-array_refs_total = [['j', 'i'], ['arr-0-0', 'brr-0', 'arr-0-1', 'arr-0-2', 'arr-0-3', 'arr-1-0', 'brr-1', 'arr-1-1', 'arr-1-2', 'arr-1-3', 'arr-2-0', 'brr-2', 'arr-2-1', 'arr-2-2', 'arr-2-3', 'arr-3-0', 'brr-3', 'arr-3-1', 'arr-3-2', 'arr-3-3']]
+def calc_array_reference_rd(array_refs_total, array_references, const_refs):
 
-array_references = ['arr-0-0', 'brr-0', 'arr-0-1', 'arr-0-2', 'arr-0-3', 'arr-0-4', 'arr-0-5', 'arr-1-0', 'brr-1', 'arr-1-1', 'arr-1-2', 'arr-1-3', 'arr-1-4', 'arr-1-5', 'arr-2-0', 'brr-2', 'arr-2-1', 'arr-2-2', 'arr-2-3', 'arr-2-4', 'arr-2-5', 'arr-3-0', 'brr-3', 'arr-3-1', 'arr-3-2', 'arr-3-3', 'arr-3-4', 'arr-3-5', 'arr-4-0', 'brr-4', 'arr-4-1', 'arr-4-2', 'arr-4-3', 'arr-4-4', 'arr-4-5']
 
-const_refs = ['j', 'i']
+# array_refs_total = [['j', 'i'], ['arr-0-0', 'brr-0', 'arr-0-1', 'arr-0-2', 'arr-0-3', 'arr-1-0', 'brr-1', 'arr-1-1', 'arr-1-2', 'arr-1-3', 'arr-2-0', 'brr-2', 'arr-2-1', 'arr-2-2', 'arr-2-3', 'arr-3-0', 'brr-3', 'arr-3-1', 'arr-3-2', 'arr-3-3']]
+# array_references = ['arr-0-0', 'brr-0', 'arr-0-1', 'arr-0-2', 'arr-0-3', 'arr-0-4', 'arr-0-5', 'arr-1-0', 'brr-1', 'arr-1-1', 'arr-1-2', 'arr-1-3', 'arr-1-4', 'arr-1-5', 'arr-2-0', 'brr-2', 'arr-2-1', 'arr-2-2', 'arr-2-3', 'arr-2-4', 'arr-2-5', 'arr-3-0', 'brr-3', 'arr-3-1', 'arr-3-2', 'arr-3-3', 'arr-3-4', 'arr-3-5', 'arr-4-0', 'brr-4', 'arr-4-1', 'arr-4-2', 'arr-4-3', 'arr-4-4', 'arr-4-5']
 
-array_references_prev = array_refs_total[1]
-const_refs_prev = array_refs_total[0]
+# const_refs = ['j', 'i']
 
-for item in const_refs:
-    if item not in const_refs_prev:
-        const_refs_prev.append(item)
+    const_refs_prev = array_refs_total[0]
+    array_references_prev = array_refs_total[1]
+    unique_Set = array_refs_total[2]
 
-const_vars = len(const_refs_prev)
+    for item in const_refs:
+        if item not in const_refs_prev:
+            const_refs_prev.append(item)
 
-rf = {}
-array_refs_total = []
-rf.setdefault(-1, 0)
+    const_vars = len(const_refs_prev)
 
-i_max = 4
-j_max = 4
+    rf = {}
+    rf.setdefault(-1, 0)
 
-possible_arrays = ['brr', 'arr']
-max_array_rfs =[]
-for item in reversed(array_references_prev):
-    if not possible_arrays:
-        break
-    else:
-        parts = item.split('-')
-        if parts[0] in possible_arrays:
-            if (len(parts) > 2):
-                max_array_rfs.append([parts[0], int(parts[1]), int(parts[2])])
-            elif(len(parts) > 1):
-                max_array_rfs.append([parts[0], int(parts[1])])
-            possible_arrays.remove(parts[0])
+    # unique_Set = set(array_references_prev)
+    cold_misses = 0
+    for item in array_references:
+        if item in unique_Set:
+            seen = set()
+            for ref in reversed(array_references_prev):
+                if ref == item:
+                    break
+                else:
+                    seen.add(ref)
+            rd = len(seen)
 
-print(max_array_rfs)
+            if rd+const_vars in rf:
+                rf[rd+const_vars] += 1
+            else:
+                rf[rd+const_vars] = 1
+            array_references_prev.remove(item)
+            array_references_prev.append(item)
+            # print(array_references_prev)
+        else:
+            cold_misses+=1
+            unique_Set.add(item)
+            array_references_prev.append(item)
+    rf[-1] +=cold_misses
+    # print(rf)
+    return rf, const_vars, array_references_prev, unique_Set
 
